39a2611ab57b13cff71d10c417f3d97e56bc5fb3
ravicuibm <tnath.ravishankar@gmail.com>
2018-09-14 22:59:08 +0530

    Ecommerce Website Done

---
+++ indextest.php
@@ -0,0 +1,179 @@
+<!DOCTYPE>
+<?php 
+session_start();
+include("functions/functions.php");
+
+?>
+<html>
+	<head>
+		<title>My Online Shop</title>
+		
+		
+	<link rel="stylesheet" href="styles/styles.css" media="all" /> 
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script>
+function showResult(str) {
+  if (str.length==0) { 
+    document.getElementById("livesearch").innerHTML="";
+    document.getElementById("livesearch").style.border="0px";
+    return;
+  }
+  if (window.XMLHttpRequest) {
+    // code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+  } else {  // code for IE6, IE5
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange=function() {
+    if (this.readyState==4 && this.status==200) {
+      document.getElementById("livesearch").innerHTML=this.responseText;
+      document.getElementById("livesearch").style.border="1px solid #A5ACB2";
+    }
+  }
+  xmlhttp.open("GET","livesearch.php?q="+str,true);
+  xmlhttp.send();
+}
+</script>
+	</head>
+	
+<body class="body">
+	
+	<!--Main Container starts here-->
+	<div class="main_wrapper">
+	
+		<!--Header starts here-->
+		<div class="header_wrapper">
+		
+			<a href="index.php"><img id="logo" src="images/l.png"  height="120" width="30%"/> </a>
+			
+		</div>
+		<!--Header ends here-->
+		
+		<!--Navigation Bar starts-->
+		<div class="menubar">
+			
+			<ul id="menu">
+				<li><a href="index.php">Home</a></li>
+				<li><a href="all_products.php">All Products</a></li>
+				<li><a href="customer/my_account.php">My Account</a></li>
+				<li><a href="#">Sign Up</a></li>
+				<li><a href="cart.php">Shopping Cart</a></li>
+				<li><a href="#">Contact Us</a></li>
+			
+			</ul>
+			
+			<div class="search-box" >
+				<form method="get" action="results.php" enctype="multipart/form-data">
+					<input type="text" size="30" onkeyup="showResult(this.value)">
+                     <div id="livesearch"></div>
+					<input type="submit" name="search" value="Search" style="float:left; margin-left:5px; margin-bottom:10px;margin-top:12px; "/>
+					
+					
+				</form>
+			
+			</div>
+			
+		</div>
+		<!--Navigation Bar ends-->
+	
+		<!--Content wrapper starts-->
+		<div class="content_wrapper" style="margin-top:50px;">
+		
+			<div id="sidebar" >
+			
+				<div id="sidebar_title">Categories</div>
+				
+				<ul id="cats">
+				
+				<?php getCats(); ?>
+				
+				<ul>
+					
+				<div id="sidebar_title">Brands</div>
+				
+				<ul id="cats">
+					
+					<?php getBrands(); ?>
+				
+				<ul>
+			
+			
+			</div>
+		
+			<div id="content_area">
+			
+			<?php cart(); ?>
+			
+			<div id="shopping_cart"> 
+					
+					<span style="float:right; font-size:17px; padding:5px; line-height:40px;">
+					
+					<?php 
+					if(isset($_SESSION['customer_email'])){
+					echo "<b>Welcome:</b>" . $_SESSION['customer_email'] . "<b style='color:yellow;'>Your</b>" ;
+					}
+					else {
+					echo "<b>Welcome Guest:</b>";
+					}
+					?>
+					
+					<b style="color:yellow">Shopping Cart -</b> Total Items: <?php total_items();?> Total Price: <?php total_price(); ?> <a href="cart.php" style="color:yellow">Go to Cart</a>
+					
+					
+					<?php 
+					if(!isset($_SESSION['customer_email'])){
+					
+					echo "<a href='checkout.php' style='color:orange;'>Login</a>";
+					
+					}
+					else {
+					echo "<a href='logout.php' style='color:orange;'>Logout</a>";
+					}
+					
+					
+					
+					?>
+					
+					
+					
+					</span>
+			</div>
+			
+				<div id="products_box">
+				
+				<?php getPro(); ?>
+				<?php getCatPro(); ?>
+				<?php getBrandPro(); ?>
+				
+				</div>
+			
+			</div>
+		</div>
+		<!--Content wrapper ends-->
+		
+		
+		
+		<div id="footer">
+		
+		<a href="#" class="fa fa-facebook"></a>
+<a href="#" class="fa fa-twitter"></a>
+<a href="#" class="fa fa-google"></a>
+<a href="#" class="fa fa-linkedin"></a>
+<a href="#" class="fa fa-youtube"></a>
+<a href="#" class="fa fa-instagram"></a>
+
+      
+		
+		</div>
+	
+	
+	
+	
+	
+	
+	</div> 
+<!--Main Container ends here-->
+
+
+</body>
+</html>
